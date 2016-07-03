a6a547dd9af407fa7e7b22959c83ebba25608c18
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-07-03 02:00:43 -0300

    server-migration.mysql: remote database dump works; added configuration file

--- mysql-server-migration.sh
+++ mysql-server-migration.sh
@@ -1,8 +1,21 @@
-# faz dump no temporario
-ssh root@site.com.br "mysqldump -u root -p'SenhaServidorTemporario' db_database2016 | gzip > /root/database.sql.tar.gz"
+#!/bin/bash
+# @summary     MySQL/MariaDB database migration to diferent server
+#
+# @description ...
+#
+# @author      Bernardo Donadio <bcdonadio@alligo.com.br>
+# @author      Emerson Rocha Luiz <emerson@alligo.com.br>
+# @copyright   Public Domain
+
+# Load configuration
+source ./mysql-server-migration.conf
+
+DATETIME=$(date +"%Y-%m-%d-%H-%M")
+
+ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysqldump -u $REMOTE_MYSQL_USER --password=$REMOTE_MYSQL_PASSWORD --host=$REMOTE_MYSQL_HOST --port=$REMOTE_MYSQL_PORT $REMOTE_MYSQL_DATABASE | gzip > ${REMOTE_MYSQL_DESTINY%+(/)}"/"$REMOTE_MYSQL_DATABASE"_"$DATETIME.sql.gz"
 
 #copia de temp para alexandria
-scp root@site.com.br:/root/database.sql.tar.gz /home/centos/database.sql.tar.gz
+#scp root@site.com.br:/root/database.sql.tar.gz /home/centos/database.sql.tar.gz
 
 # importa no alexandria
-zcat /home/centos/database.sql.tar.gz | mysql -u 'root' -p'SenhaServidorDestino' site_site
+#zcat /home/centos/database.sql.tar.gz | mysql -u 'root' -p'SenhaServidorDestino' site_site
