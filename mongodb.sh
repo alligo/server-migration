f34cedc329b843d41ebe936995fbf3808664c288
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-07-03 05:07:33 -0300

    mongodb: created mongo_remotedump()

--- mongodb.sh
+++ mongodb.sh
@@ -29,6 +29,39 @@ mongo_localdump() {
   mongodump --host $1 --port $2 --out $3
   tar -zcf ${3%/}".tar.gz" -C $3 .
   rm -rf $3
+  # todo: maybe just do not remove temporary folder?
+  #       problem is that it could do not remove deleted content
+  # todo: use temp folder and just use destiny to tar.gz file
+}
+
+mongo_localrestore() {
+  # @todo finish this
+}
+
+# mongo_remotedump:
+# 
+# $1 = ssh_user
+# $2 = ssh_host | ssh_host:port
+# $3 = mongoport
+# $4 = backupfolder (no timestamp for temporary folder)
+mongo_remotedump() {
+  # Function not tested
+  DATETIME=$(date +"%Y-%m-%d-%H-%M")
+  ssh ${1}@${2} "mongodump --port $3 --out $4"
+  ssh ${1}@${2} "tar -zcf ${4%/}${DATETIME}".tar.gz" -C $4 ."
+  #Note: this function will not rm -rf directory. Just to avoid problems
+}
+
+mongo_remoterestore() {
+  # @todo finish this
+}
+
+move_local2remote() {
+  # @todo finish this
+}
+
+move_remote2local() {
+  # @todo finish this
 }
 
 
